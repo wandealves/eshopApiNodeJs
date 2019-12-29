0c8f231877bbd5c6f1db40fb60e3de91d800be58
Wanderson <wanderson.alves.rodrigues@gmail.com>
2019-12-29 13:24:11 -0300

    Adição e configuração do0 sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: '192.168.1.108',
+    username: 'wanderson',
+    password: 'root',
+    database: 'eshop',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
