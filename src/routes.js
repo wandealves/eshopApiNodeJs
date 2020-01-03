e1814a166a377c36457d38cb2ca04cb1b9a9396c
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-01-03 14:53:18 -0300

    Post Users

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'wanderson',
-        email: 'wanderson.alves.rodrigues@gmail.com',
-        password_hash: '12345',
-    });
-    res.json(user);
-});
+routes.post('/api/users', UserController.post);
 
 export default routes;
