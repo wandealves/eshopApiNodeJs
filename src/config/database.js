1529c783bbf5ce6a67a0a622b6488ff6c9fe2c46
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-01-03 13:27:01 -0300

    Criação de acesso database

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     dialect: 'postgres',
-    host: '192.168.1.108',
+    host: '192.168.1.102',
     username: 'wanderson',
     password: 'root',
     database: 'eshop',
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'wanderson',
+        email: 'wanderson.alves.rodrigues@gmail.com',
+        password_hash: '12345',
+    });
+    res.json(user);
+});
 
 export default routes;
