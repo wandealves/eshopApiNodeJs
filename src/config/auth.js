8d7e7e52a843ae47c8bf5957d0c1fc1a90a6e894
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-01-03 15:28:31 -0300

    Create jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'ca2e74e48f7db2cc5d927d56db33b409',
+    expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/api/users', UserController.post);
+routes.post('/api/sessions', SessionController.post);
 
 export default routes;
